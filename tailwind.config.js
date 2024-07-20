5f3210fe1ca1b3f1507b5d960abd59d00591137e
Muhammad Yusuf Arrofi <dangouu123@gmail.com>
2024-07-20 20:57:57 +0700

    menambahkan About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,28 @@
+/**
+ * Hallo semua, projek ini saya buat untuk latihan TailwindCSS
+ * Saya menggunakan TailwindCSS versi 3.0.24
+ * Saya membuat ini untuk Portfolio saya, 
+ * 
+ * 
+ * @NXRts
+ */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
   theme: {
     container: {
       center: true,
-      padding: '20px'
+      padding: '40px'
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        dark: '#0f172a',
+        primary: '#1f2937',
         secondary: '#64748b',
+        dark: '#0f172a',
       },
       screens: {
-        '2xl': '1220px',
+        '2xl': '1320px',
       }
     },
   },
